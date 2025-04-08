a2041381025e5ce281b259a643679672919a818c
Sanjaya <primasan@lm4-945-31963.local>
2025-04-08 14:47:35 +0300

    wes benchmark only

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ def download_checkpoint(url,name,extract=False):
 shell_scripts = [os.path.join('muat/pkg_shell', f) for f in os.listdir('muat/pkg_shell') 
                 if f.endswith('.sh')]
 
-download_checkpoint("https://huggingface.co/primasanjaya/muat-checkpoint/resolve/main/best_wes_tcga.zip",'tcga_wes.zip',False)
+#download_checkpoint("https://huggingface.co/primasanjaya/muat-checkpoint/resolve/main/best_wes_tcga.zip",'tcga_wes.zip',False)
 download_checkpoint("https://huggingface.co/primasanjaya/muat-checkpoint/resolve/main/benchmark_wes_slim.zip",'benchmark_wes_slim.zip',False)
 
 #download genome reference and unzip 
