73fd55efb1e076a2ec47a3f7e90513cacb06f188
primasan <primasan@ad.helsinki.fi>
2026-03-17 16:31:47 +0200

    before 0.15

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ path = os.path.dirname(os.path.abspath(__file__)) + '/'
 
 setup(
     name="muat",
-    version="0.1.13",
+    version="0.1.15",
     packages=find_packages(),
     package_data={
         'muat': [
