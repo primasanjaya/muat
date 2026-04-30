f8e9b91280934b8292a97684b5d94ef66d10e408
primasan <primasan@ad.helsinki.fi>
2026-04-30 11:15:55 +0300

    v.0.18

--- muat/__init__.py
+++ muat/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.17'
+__version__ = '0.1.18'
 
 '''
 # Import specific functions you want to expose
--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ path = os.path.dirname(os.path.abspath(__file__)) + '/'
 
 setup(
     name="muat",
-    version="0.1.17",
+    version="0.1.18",
     packages=find_packages(),
     package_data={
         'muat': [
