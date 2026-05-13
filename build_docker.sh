68ad0769bf9ee7fc9f835ba15dc7dfbbda190a13
primasan <primasan@ad.helsinki.fi>
2026-05-13 12:43:04 +0300

    released v.0.1.20

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,5 @@
 #build docker for linux amd architecture 
-docker build --platform linux/amd64 -t muat:v0.1.17.1 .
+docker build --platform linux/amd64 -t muat:v0.1.20 .
 
 #run and test it
-#docker run -it muat:v0.1.17-c muat -h
+#docker run -it muat:v0.1.20 muat -h
